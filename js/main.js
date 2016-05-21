f271b5bdebca49fe31103ced724dca158daa371d
Angga Ari Wijaya <anggadarkprince@gmail.com>
2016-05-22 06:27:01 +0700

    add google analytic and fix blog url

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 jQuery(document).ready(function () {
+
+    // Google Analytic
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    ga('create', 'UA-74498739-2', 'auto');
+    ga('send', 'pageview');
+
     // 2/3/4th level menu  offscreen fix
     var MainWindowWidth = jQuery(window).width();
     jQuery(window).resize(function () {
